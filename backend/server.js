2e4a78324fc847d15852dfdb2ea571da9451084e
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-03-08 18:18:04 +0800

    Add axios migration guide and update package version

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express'; 
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
+import products from './data/products.js';
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: 'Product Not Found' });
+  }
+});
+
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
+});
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,15 +1,28 @@
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+// import products from '../products';
+import axios from 'axios';
 
 
 const Product = () => {
-  const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
-  console.log(product);
 
+  const [product, setProduct] = useState([]);
+
+  const { id: productId } = useParams();
+  // const product = products.find((p) => p._id === productId);
+  // console.log(product);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    }
+    fetchProducts();
+  
+  }, [productId]);
   return (
     <>
       <Link className='btn btn-light my-3 fw-medium text-body' to='/'>Go back</Link>
