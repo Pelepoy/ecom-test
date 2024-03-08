42a71c629d5a39a6e18cd0366e85f92e66e74ddb
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-03-08 15:04:51 +0800

    Refactor App.js component structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
-import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen';
-import { Outlet } from 'react-router-dom';
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import { Outlet } from "react-router-dom";
 
-const App = () => { 
+const App = () => {
   return (
     <>
       <Header />
-      <main className='py-3'>
-        <Container>
-          <h1>Welcome to E-COM TEST</h1>
-          <Outlet />
-        </Container>
-      </main>
+      <div className="main">
+        <main className="py-3">
+          <Container>
+            <h1>Welcome to E-COM TEST</h1>
+            <Outlet />
+          </Container>
+        </main>
+      </div>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
