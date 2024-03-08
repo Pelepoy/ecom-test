32b4b0fc78157282c3c28a7507f1d1cb012fdb77
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-03-08 13:00:24 +0800

    Refactor routing in App.js and index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Container } from 'react-bootstrap';
 import Header from "./components/Header";
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import { Outlet } from 'react-router-dom';
 
 const App = () => { 
   return (
@@ -10,7 +11,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <h1>Welcome to E-COM TEST</h1>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
